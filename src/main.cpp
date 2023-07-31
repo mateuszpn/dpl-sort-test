c4623039ae641e18e7d9b92d198146812bba6b5f
Mateusz P. Nowak <mateusz.p.nowak@intel.com>
2023-07-31 05:42:43 -0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,13 @@ using namespace std;
 template <typename T>
 // void generate_random(T *v, std::size_t n, std::size_t bound = 100) {
 void generate_random(std::vector<T> &v, std::size_t n, std::size_t bound = 100) {
+  for (std::size_t i = 0; i < n; i++) {
+    v[i] = lrand48() % bound;
+  }
+}
 
+template <typename T>
+void generate_random(span<T> &v, std::size_t n, std::size_t bound = 100) {
   for (std::size_t i = 0; i < n; i++) {
     v[i] = lrand48() % bound;
   }
@@ -31,6 +37,9 @@ int main(int argc, char **argv) {
   using T = int;
   std::vector<std::size_t> sizes = { 4, 7, 23 };
 
+  // auto policy = oneapi::dpl::execution::dpcpp_default;
+  auto policy = oneapi::dpl::execution::make_device_policy(sycl::queue());
+
   int rank = 0, size = 0;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -39,12 +48,11 @@ int main(int argc, char **argv) {
   for (std::size_t n : sizes) {
     std::cout << rank << ": >>> Test size " << n << std::endl;
 
-    sycl::queue q;
+    std::vector<T> sv(n);
 
-    std::vector<T> sv(n); 
-
-    // T *sv = std::allocator<T>().allocate(n);
-    // T *sv = sycl::usm_allocator<T, sycl::usm::alloc::shared, 0>(q).allocate(n); // aka: mhp::default_allocator
+    // T *s = std::allocator<T>().allocate(n);
+    // T *s = sycl::usm_allocator<T, sycl::usm::alloc::shared, 0>(sycl::queue()).allocate(n); /* aka: mhp::default_allocator */
+    // auto sv = span<T>(s, n);
 
     generate_random(sv, n, 100);
 
@@ -54,10 +62,6 @@ int main(int argc, char **argv) {
     }
     std::cout << "\n";
 
-    // auto policy = oneapi::dpl::execution::dpcpp_default;
-    auto policy = oneapi::dpl::execution::make_device_policy(q);
-
-    // oneapi::dpl::sort(policy, sv, sv + n);
     oneapi::dpl::sort(policy, sv.begin(), sv.end());
 
     bool sorted = true;
@@ -78,9 +82,9 @@ int main(int argc, char **argv) {
     }
     std::cout << "\n";
 
-    // std::allocator<T>().deallocate(sv, n);
-    // sycl::usm_allocator<T, sycl::usm::alloc::shared, 0>(q).deallocate(sv, n);
-    
     MPI_Barrier(MPI_COMM_WORLD);
+
+    // std::allocator<T>().deallocate(s, n);
+    // sycl::usm_allocator<T, sycl::usm::alloc::shared, 0>(q).deallocate(s, n);
   }
 }
